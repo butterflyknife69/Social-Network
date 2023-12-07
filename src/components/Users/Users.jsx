8aaf15c143769fa55c454a24d3b2a2f99457e950
Igor <kishmashine@gmail.com>
2023-12-07 20:11:09 +0200

    Side Effect/52

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,20 +4,22 @@ import userPhoto from '../../assets/imges/User.png'
 
 let Users = (props) => {
 
-  if (props.users.length === 0) {
-
-    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-      props.setUsers(response.data.items)
-    })
+  let getUsers = () => {
+    if (props.users.length === 0) {
+      axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        props.setUsers(response.data.items)
+      })
+    }
   }
 
   return (
     <div className={s.div} >
+      <button onClick={getUsers}> Get Users</button>
       {
         props.users.map(u => <div key={u.id}>
           <span>
             <div>
-              <img src={u.photos.small != null ? u.photos.small:userPhoto} className={s.userPhoto} alt="img" />
+              <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto} alt="img" />
             </div>
             <div>
               {u.followed
